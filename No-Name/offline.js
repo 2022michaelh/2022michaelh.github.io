7d2e7b276591f47476a2d4c1ad2fcc5aad4aefc5
2022michaelh <44642882+2022michaelh@users.noreply.github.com>
2019-06-06 10:01:18 -0400

    Fix

---
+++ No-Name/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1559829000,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background1.png",
+		"images/background3.png",
+		"images/player-sheet0.png",
+		"images/background2.png",
+		"images/playergun-sheet0.png",
+		"images/mousesprite-sheet0.png",
+		"images/mouseimage-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/laserhit-sheet0.png",
+		"images/lasermark-sheet0.png",
+		"images/construct2logo-sheet0.png",
+		"images/flashlightlight-sheet0.png",
+		"images/ghost-sheet0.png",
+		"images/ourlogo-sheet0.png",
+		"images/gameoverbackground-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
